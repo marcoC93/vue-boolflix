e52b23597cbdc24b71d8c109793179cbc6f93fd9
Marco <marco.chianese93@gmail.com>
2020-11-25 14:30:31 +0100

    bugfix errore 401 API

--- script/script.js
+++ script/script.js
@@ -10,15 +10,15 @@ var app = new Vue( {
           axios.get('https://api.themoviedb.org/3/search/movie', {
               params : {
 
-                apiKey : 'a837f0e3cc53c9e644cd012f87fc7331',
-                query : this.query ,
+                api_key : 'a837f0e3cc53c9e644cd012f87fc7331',
+                query : this.query
               }
 
-        })
-        .then(response => {
-              console.log(response);
+        } )
+        .then((results) => {
+              console.log(results);
 
-        })
+        });
 
 
 
